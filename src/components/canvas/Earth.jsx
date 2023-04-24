ede10283770519b94ce103bd48709b221146c546
Zaeem Shakir <shakirzaeem@gmail.com>
2023-04-24 16:51:09 -0700

    final commit

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
-
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber"; //gives us an empty canvas to work with
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"; //these help us to draw on canvas use gltf allow us to import 3d models
+import CanvasLoader from "../Loader"
 const Earth = () => {
+  const earth = useGLTF("./planet/scene.gltf");
   return (
-    <div>Earth</div>
+    <primitive object={earth.scene}
+    scale={2.5}
+    position-y={0}
+    rotation-y={0}
+
+    />
   )
 }
 
-export default Earth
+const BallCanvas = () => {
+  return (
+    <Canvas
+     frameloop="demand"
+      shadows
+      camera={{ fov:45,near:0.1,far:200,position:[-4,3,6]}}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+        autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth  />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+export default BallCanvas
