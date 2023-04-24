571950080e7f9949f46aa21564b3490e8779934d
Zaeem Shakir <shakirzaeem@gmail.com>
2023-04-23 20:27:52 -0700

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../style";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav
+      className={`${styles.padding6} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0); //scroll top of page
+          }}
+        >
+          <img src={logo} alt="logo" className="w-12 h-12 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Portfolio
+          </p>
+        </Link>
+        <ul className=" list-none hidden  sm:flex flex-row gap-10">
+          {navLinks.map((Link) => (
+            <li
+              key={Link.id}
+              className={`${
+                active === Link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+            onClick={()=>setActive(Link.title)}
+            
+              >
+              <a href={`#${Link.id}`}>{Link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center" >
+        <img src={menu} alt="menu"
+        className="w-[28px] h-[28px] object-contain cursor-pointer"
+        onClick={()=>setToggle(!toggle)}
+        />
+        </div>
+        </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
